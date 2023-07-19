c7c624439ee021ffcd7cd37505c45b692de292f8
Levantine-1 <github@levantine.io>
2023-07-18 22:14:45 -0700

    fixing filter

--- roles/os_configs/filter_plugins/get_host_ip.py
+++ roles/os_configs/filter_plugins/get_host_ip.py
@@ -1,15 +1,15 @@
 # filter_plugins/get_host_ip.py
 
+def get_host_ip(content, hostname):
+    lines = content.splitlines()
+    for line in lines:
+        parts = line.split()
+        if len(parts) >= 2 and parts[-1] == hostname:
+            return parts[0]
+    return None
+
 class FilterModule(object):
     def filters(self):
         return {
-            'get_host_ip': self.get_host_ip
+            'get_host_ip': get_host_ip
         }
-
-    def get_host_ip(self, content, hostname):
-        lines = content.splitlines()
-        for line in lines:
-            parts = line.split()
-            if len(parts) >= 2 and parts[-1] == hostname:
-                return parts[0]
-        return None
